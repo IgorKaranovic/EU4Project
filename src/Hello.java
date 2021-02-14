616ccd2a0c9c54971c50ec9613dcf651a986fda6
Igor Karanovic <igor.karanovic21@gmail.com>
2021-02-14 18:07:49 +0100

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,10 @@ public class Hello {
     //more stuff
     //Windows user and Mac users are happy to see commit dialog finally
 
+    //I have already published on Github.com
+    //now i am ready to make more commits
+    //but i do need to share again
+    //Absolutely no
+    //It's time commit and just push!!!
+
 }
